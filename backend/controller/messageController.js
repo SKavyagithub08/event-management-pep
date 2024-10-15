f18b6f6ff0d544ca119a26d11ce4a3567c0897db
SKavyagithub08 <kavyasri2330@gmail.com>
2024-10-15 21:59:48 +0530

    feat: updated

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -22,10 +22,31 @@ export const sendMessage = async (req, res) => {
         });
     } 
     catch (error) {
+
+        if(error.name === "ValidationError"){
+            let errorMessage = "";
+            if (error.errors.name) {
+                errorMessage += error.errors.name.message + " ";
+            }
+            if(error.errors.email){
+                errorMessage += error.errors.email.message + " ";
+            }
+            if(error.errors.subject){
+                errorMessage += error.errors.subject.message + " ";
+            }
+            if(error.errors.message){
+                errorMessage += error.errors.message.message + " ";
+            }
+            return res.status(200).json({
+                success: false,
+                message: errorMessage,
+            });
+        }
+
         // Handle internal server errors
         return res.status(500).json({
             success: false,
-            error: error.message || "Internal server error",
+            message: "Unknown Error",
         });
     }    
 };
